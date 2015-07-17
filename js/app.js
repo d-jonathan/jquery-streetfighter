08925adfa9bfc518701998e06ba4cee44e5d7ae8
Jonathan DeFaveri <d.jonathan@gmail.com>
2015-07-17 17:58:50 -0400

    hadouken sound added

--- js/app.js
+++ js/app.js
@@ -8,16 +8,27 @@ $(document).ready(function () {
 		$('.ryu-still').show();
 	})
 	.mousedown(function () {
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show();
-		// play hadouken sound
-		// show hadouken and animate it to the right
+		$('.hadouken').finish().show().animate(
+			{'left': '1059px'},
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('left', '559px');
+			}
+		);
 	})
 	.mouseup(function () {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
-		// ryu goes back to his ready position
 	});
 
 });
+
+function playHadouken () {
+	$('#hadouken-sound')[0].volume=0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
